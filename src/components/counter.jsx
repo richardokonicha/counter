c591d87d30540949fc5701acc368f5ae1dcc5f65
reechee <richardokonicha@gmail.com>
2020-06-16 10:39:43 +0100

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component {
+    render() { 
+        return ( <h1>I am a Counter element- mosh at time = 30</h1> );
+    }
+}
+ 
+export default Counter;
